0db1482875d12dd52de607aa9a5fe6ac44e4db6b
Arnaud Botella <arnaud.botella@geode-solutions.com>
2020-01-14 09:53:27 +0100

    Update memory_resource.h

--- include/bitsery/ext/utils/memory_resource.h
+++ include/bitsery/ext/utils/memory_resource.h
@@ -134,27 +134,28 @@ namespace bitsery {
                 using const_reference = const T&;
                 using size_type = size_t;
                 using difference_type = ptrdiff_t;
+                
                 size_t max_size() const noexcept {
-    return std::numeric_limits<size_t>::max() / sizeof(value_type);
-}
+                    return std::numeric_limits<size_t>::max() / sizeof(value_type);
+                }
 
-void construct(T *p, const T &val) {
-    new((void *) p) T(val);
-}
+                void construct(T *p, const T &val) {
+                    new((void *) p) T(val);
+                }
 
-template<class U, class... Args>
-void construct(U *p, Args &&... args) {
-    new((void *) p) U(std::forward<Args>(args)...);
-}
+                template<class U, class... Args>
+                void construct(U *p, Args &&... args) {
+                    new((void *) p) U(std::forward<Args>(args)...);
+                }
 
-void destroy(T *p) {
-    p->~T();
-}
+                void destroy(T *p) {
+                    p->~T();
+                }
 
-template<class U>
-void destroy(U *p) {
-    p->~U();
-}
+                template<class U>
+                void destroy(U *p) {
+                    p->~U();
+                }
 
                 template<typename U>
                 struct rebind {
