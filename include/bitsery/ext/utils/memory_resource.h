0e76e0608c3140e5538b98fc4fc60ac7e5d0bc25
Arnaud Botella <arnaud.botella@geode-solutions.com>
2020-01-17 07:08:49 +0100

    Fix compilation with gcc 4.8.2 (#33)
    Update for gcc4.8.2 for Centos7

--- include/bitsery/ext/utils/memory_resource.h
+++ include/bitsery/ext/utils/memory_resource.h
@@ -24,6 +24,7 @@
 #define BITSERY_EXT_MEMORY_RESOURCE_H
 
 #include "../../details/serialization_common.h"
+#include <cstddef>
 #include <new>
 
 namespace bitsery {
@@ -127,6 +128,39 @@ namespace bitsery {
             class StdPolyAlloc {
             public:
                 using value_type = T;
+                using pointer = T*;
+                using const_pointer = const T*;
+                using reference = T&;
+                using const_reference = const T&;
+                using size_type = size_t;
+                using difference_type = ptrdiff_t;
+                
+                size_t max_size() const noexcept {
+                    return std::numeric_limits<size_t>::max() / sizeof(value_type);
+                }
+
+                void construct(T *p, const T &val) {
+                    new((void *) p) T(val);
+                }
+
+                template<class U, class... Args>
+                void construct(U *p, Args &&... args) {
+                    new((void *) p) U(std::forward<Args>(args)...);
+                }
+
+                void destroy(T *p) {
+                    p->~T();
+                }
+
+                template<class U>
+                void destroy(U *p) {
+                    p->~U();
+                }
+
+                template<typename U>
+                struct rebind {
+                    using other = StdPolyAlloc<U>;
+                };
 
                 explicit constexpr StdPolyAlloc(MemResourceBase* memResource)
                     :_alloc{memResource} {}
